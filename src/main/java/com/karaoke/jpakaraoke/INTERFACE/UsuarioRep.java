ffaebc5d40d8b8e2062025d65b96795c2b606e95
joshDev27 <162900975+joshDev27@users.noreply.github.com>
2024-05-24 20:39:08 -0500

    se actualiza para sp UsuarioRep.java

--- src/main/java/com/karaoke/jpakaraoke/INTERFACE/UsuarioRep.java
+++ src/main/java/com/karaoke/jpakaraoke/INTERFACE/UsuarioRep.java
@@ -2,12 +2,15 @@ package com.karaoke.jpakaraoke.INTERFACE;
 
 import com.karaoke.jpakaraoke.ENTIDAD.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import org.springframework.data.jpa.repository.query.Procedure;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UsuarioRep extends JpaRepository<Usuario,Integer> {
 
-       // Método para invocar el SP
+     // Método para invocar el SP
      @Procedure(name = "sp_karaoke_login")
      int sp_karaoke_login(@Param("nombre") String nombre, @Param("contrasena") String contrasena);
 }
