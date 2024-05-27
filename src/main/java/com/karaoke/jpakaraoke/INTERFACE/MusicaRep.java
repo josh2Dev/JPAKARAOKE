8712df61fce512288bd91827f55ad7218e8a1e22
joshDev27 <162900975+joshDev27@users.noreply.github.com>
2024-05-27 18:44:47 -0500

    Distinct.java

--- src/main/java/com/karaoke/jpakaraoke/INTERFACE/MusicaRep.java
+++ src/main/java/com/karaoke/jpakaraoke/INTERFACE/MusicaRep.java
@@ -3,7 +3,14 @@ package com.karaoke.jpakaraoke.INTERFACE;
 import com.karaoke.jpakaraoke.ENTIDAD.Musica;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.Query;
+import java.util.List;
+
+
 
 @Repository
 public interface MusicaRep extends JpaRepository<Musica,Integer> {
+
+    @Query("SELECT DISTINCT m.generoMusica FROM Musica m")
+    List<String> findDistinctGeneroMusica();
 }
